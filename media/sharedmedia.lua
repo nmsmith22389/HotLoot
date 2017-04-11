dd83700644a6c9624679f680d52b5099096f0753
Neil Smith <nmsmith22389@gmail.com>
2017-04-10 23:08:26 -0500

    Added fonts
    Added fonts

---
+++ media/sharedmedia.lua
@@ -0,0 +1,8 @@
+local LSM = LibStub('LibSharedMedia-3.0')
+
+-- Get Font -->
+-- LSM:Fetch('font', self.db['general'].font)
+-- LSM:Fetch('font', 'Macondo')
+
+LSM:Register('font', 'Macondo'              , [[Interface\AddOns\HotLoot\media\fonts\Macondo-Regular.ttf]])
+LSM:Register('font', 'Roboto Condensed Bold', [[Interface\AddOns\HotLoot\media\fonts\RobotoCondensed-Bold.ttf]])
